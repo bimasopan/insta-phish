36a3b02a1a321ef744d2ccd6a68c4055ac9d17db
i'm vector from indonesia <130651966+bimasopan@users.noreply.github.com>
2023-12-22 14:05:14 +0700

    Add files via upload

---
+++ template/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo "installing express js"
+sleep 2
+npm install express
+clear
+echo "installing fs"
+sleep 2
+npm install fs
+clear
+echo "installing path"
+sleep 2
+npm install path
+clear
+echo "instaliing body-parser"
+sleep 2
+npm install body-parser
