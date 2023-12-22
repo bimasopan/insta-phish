706d4086300a52f619a08644907f275c1f172b89
i'm vector from indonesia <130651966+bimasopan@users.noreply.github.com>
2023-12-22 11:49:16 +0700

    Update server.sh
    menambahkan pengecekan sistem operasi

--- server.sh
+++ server.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# xterm -e 'php -S localhost:8080' & sleep 2; xterm -e 'ngrok http 80'
-gnome-terminal --tab -- bash -c 'php -S localhost:8080'
-sleep 2
-gnome-terminal --tab -- bash -c 'ngrok http 8080'
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    gnome-terminal --tab -- bash -c 'php -S localhost:8080'
+    sleep 2
+    gnome-terminal --tab -- bash -c 'ngrok http 8080'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    start cmd /k "php -S localhost:8080"
+    timeout 2 > nul
+    start cmd /k "ngrok http 8080"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    start cmd /k "php -S localhost:8080"
+    timeout 2 > nul
+    start cmd /k "ngrok http 8080"   
+else
+    echo "System not recognized"
+fi
